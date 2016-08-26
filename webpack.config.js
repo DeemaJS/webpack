2d4a7a58345f7094dcb24f133d40224a4b1cc7de
DeemaJS <deeema@outlook.com>
2016-08-26 18:24:17 +0300

    Add two entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,63 @@
 'use strict';
 
-var NODE_ENV = process.env.NODE_ENV || 'development';
-var webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./dist/home.js",
-  output: {
-    filename: "./dist/build.js",
-    library: "home"
+  context: __dirname + '/frontend',
+
+  entry: {
+    home:  "./home",
+    about: "./about"
   },
 
- watch: NODE_ENV == 'development',
-   watchOptions: {
+  output: {
+    path:     __dirname + '/public',
+    filename: "[name].js",
+    library:  "[name]"
+  },
+
+  watch: NODE_ENV == 'development',
+
+  watchOptions: {
     aggregateTimeout: 100
   },
-   devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : null,
-   plugins: [
+
+  devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : null,
+
+  plugins: [
+    new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(NODE_ENV),
       LANG:     JSON.stringify('ru')
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "common"
     })
   ],
-   resolve: {
+
+  resolve: {
     modulesDirectories: ['node_modules'],
     extensions:         ['', '.js']
   },
-   resolveLoader: {
+
+  resolveLoader: {
     modulesDirectories: ['node_modules'],
     moduleTemplates:    ['*-loader', '*'],
     extensions:         ['', '.js']
   },
-    module: {
-     loaders: [{
-      test:   /\.js$/,
-      loader: 'babel?presets[]=es2015'
-    }]
-   }
 
+
+  module: {
+
+    loaders: [{
+      test:   /\.js$/,
+      loader: 'babel?optional[]=runtime'
+    }]
+
+  }
 };
+
 
 if (NODE_ENV == 'production') {
   module.exports.plugins.push(
