47af558b6904f190d39bbab01a71afad5966ef05
DeemaJS <deeema@outlook.com>
2016-09-12 17:04:27 +0300

    Add cashing with hash

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const rimraf = require('rimraf');
 
 function addHash(template, hash) {
   return NODE_ENV == 'production' ?
-      template.replace(/\.[^.]+$/, `.[${hash}]$&`) : `${template}?hash=[${hash}]`;
+      template.replace(/\.[^.]+$/, `.[${hash}]$&`) : template; /*`${template}?hash=[${hash}]`*/
 }
 
 module.exports = {
