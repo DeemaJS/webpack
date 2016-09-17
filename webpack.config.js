712e949aa72bf7331f5ac175bad06bbcfe70cae6
DeemaJS <deeema@outlook.com>
2016-09-17 18:44:00 +0300

    Add live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 'use strict';
 
-let webpack = require('webpack');
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: __dirname + '/frontend',
-  entry:   {
+
+  entry: {  // --inline --hot
     main: './main'
   },
-  output:  {
+
+  output: {
     path:       __dirname + '/public',
     publicPath: '/',
     filename:   '[name].js'
@@ -17,58 +19,29 @@ module.exports = {
   module: {
 
     loaders: [{
-      test:   /\.js$/,
+      test:    /\.js$/,
       include: __dirname + '/frontend',
-      loader: "babel?presets[]=es2015"
+      loader:  "babel?presets[]=es2015"
     }, {
       test:   /\.jade$/,
       loader: "jade"
     }, {
       test:   /\.styl$/,
-      loader: 'style!css!stylus?resolve url'
+      loader: ExtractTextPlugin.extract('style', 'css!stylus?resolve url')
     }, {
       test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-      loader: 'file?name=[name].[ext]?[hash]'
+      loader: 'file?name=[path][name].[ext]?[hash]'
     }]
 
   },
 
+  plugins: [
+    new ExtractTextPlugin('[name].css', {allChunks: true, disable: process.env.NODE_ENV=='development'})
+  ],
+
   devServer: {
-
-    host: 'localhost', // default
-    port: 8080, // default
-
-    contentBase: __dirname + '/backend', // static files, cwd() by default, false to disable
-    historyApiFallback: true // if static file not found, go /index.html
-
-
-
-    /*
-
-    // middlware ->
-    //   proxy ->
-    //     historyApiFallback ? -> historyApiFallback, middleware
-    //       -> contentBase
-
-    proxy: [{ // array in proxy allows regexp path so using it
-      path: "dynamic/* or /regexp/",
-      host: "http://proxy.host", // if another HOST header needed for proxy,
-      bypass: function(req, res, options) {
-        // return URL to rewrite req.url and SKIP PROXY
-        // return false otherwise
-      },
-      rewrite: function(req, options) {
-        // do something with req if needed
-      },
-      configure: function(proxy) {
-        // do something with http-proxy server instance if needed (add handlers etc)
-      }
-    }],
-
-    contentBase: __dirname + '/backend', // static files, cwd() by default, false to disable
-    historyApiFallback: true // if static file not found, go /index.html
-
-     */
+    contentBase: __dirname + '/backend',
+    hot: true
   }
 };
 
